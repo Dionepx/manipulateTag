f5c9dec29868525d773f4f88ada00ef12936117f
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-03-27 22:22:21 -0300

    Add files via upload

---
+++ manipulateTag/script.js
@@ -0,0 +1,14 @@
+// criar
+// identificar (id/class)
+// resgatar (DOM)
+// criar uma funcao
+
+const idTxt = document.getElementById('txt')
+
+const myFunc = () => { 
+     idTxt.innerHTML += `
+          <div>
+               <h3>Teste</h3>
+          </div>
+     `
+}
